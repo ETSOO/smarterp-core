a2ba87da6f93cd09aaec81c3f5779b14bacca1ac
Garry Xiao <xz@etsoo.com>
2025-06-10 21:44:37 +1200

    Add avatarStyles to CoreUtils

--- src/CoreUtils.ts
+++ src/CoreUtils.ts
@@ -1,4 +1,17 @@
 /**
  * Core utilities
  */
-export namespace CoreUtils {}
+export namespace CoreUtils {
+  /**
+   * Get avatar styles
+   * @param isOrg Is this an organization avatar?
+   * @returns Styles
+   */
+  export function avatarStyles(isOrg: boolean = false): React.CSSProperties {
+    return {
+      width: "160px",
+      height: isOrg ? "80px" : "160px",
+      border: "1px solid #666"
+    };
+  }
+}
